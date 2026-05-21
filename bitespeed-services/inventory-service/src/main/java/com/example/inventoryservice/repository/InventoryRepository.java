cdb170aacb64e6a3a1cbbd03792fcb307cf2c53c
malithrashmika <malithrashmika10@gmail.com>
2026-05-22 04:32:31 +0530

    Remove local project dirs before adding as submodules

--- bitespeed-services/inventory-service/src/main/java/com/example/inventoryservice/repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.inventoryservice.repository;
-
-import com.example.inventoryservice.entity.Inventory;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InventoryRepository extends MongoRepository<Inventory, String> {
-}
